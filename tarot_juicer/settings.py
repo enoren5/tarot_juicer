5c9c7aec4b386591679955100ffa91598a308975
Charles Robert Calcifer <coffee.drinker.daniel@gmail.com>
2019-12-03 15:06:33 -0500

    Initialize static dir

--- tarot_juicer/settings.py
+++ tarot_juicer/settings.py
@@ -119,5 +119,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATIC_FILES_DIRS = [
+    os.path.join(BASE_DIR, 'tarot_juicer/static')
+]
