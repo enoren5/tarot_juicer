1976ee9fbcf98abab847c2cbb706596756b8ccbc
Charles Robert Calcifer <coffee.drinker.daniel@gmail.com>
2019-12-03 14:54:48 -0500

    Complete 'portal' - - homepage fully operational

--- landings/urls.py
+++ landings/urls.py
@@ -4,5 +4,5 @@ from . import views  # , include
 urlpatterns = [
     # path('', views.index, name='index'),
     path('about', views.about, name='about'),
-    path('portal', views.portal, name='portal'),
+    path('', views.portal, name='portal'),
 ]
