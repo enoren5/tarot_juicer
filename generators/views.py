71c1bc8b7520f550fe836ec30272b96f85480970
Rakeen Ali <rakeenali20@gmail.com>
2019-12-26 23:19:59 +0500

    excetions handled

--- generators/views.py
+++ generators/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
 from .models import Generator
 
 
 def at_random(request):
-    generator = Generator.objects.order_by('?').first()
-    context = {
-        'generator': generator,
-    }
+    try:
+        generator = Generator.objects.order_by('?').first()
+        context = {
+            'generator': generator,
+        }
+    except ObjectDoesNotExist:
+        context = {
+            'generator': None
+        }
 
     return render(request, 'generators/at_random.html', context)
