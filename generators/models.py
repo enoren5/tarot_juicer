8315bfc83eab7c654a847e33ea1bbf7963f6c75e
Rakeen Ali <rakeenali20@gmail.com>
2019-12-26 23:05:55 +0500

    updated models,py

--- generators/models.py
+++ generators/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Generator(models.Model):
     title = models.CharField(max_length=24)
     number = models.IntegerField()
-    # tarot_card_image must be an html box to contain a URL reference because the image data must be delegated/outsourced to imgur to save on bandwidth rather than serving the tarot_card_image data locally
+    # tarot_card_image must be an html CharField to contain a URL reference because the image data must be delegated/outsourced to imgur to save on bandwidth rather than serving the tarot_card_image data locally
     tarot_card_image = models.CharField(max_length=1024)
     astrological = models.CharField(max_length=140)
     alchemical = models.CharField(max_length=140)
