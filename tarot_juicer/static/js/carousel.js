1d9da69059d3c405046e08a90f38ca5cab443aa7
Michael Esch <eschmichael@yandex.com>
2019-12-28 17:49:18 -0500

    add starting point for carousel js

---
+++ tarot_juicer/static/js/carousel.js
@@ -0,0 +1,17 @@
+/*
+CSS reference:
+main div that contains the carousel
+    .carousel
+
+div that contains the carousel track
+    .carousel_track-container
+
+ul - contains tracks as li
+    .carousel_track
+
+li one per slide / image
+    .carousel_slide
+
+Individual img contained in slide
+    .carousel_image
+*/
