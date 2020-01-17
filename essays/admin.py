a1f3748564d72ea79a8ff54a0674b8d32c775cae
Charles Robert Calcifer <coffee.drinker.daniel@gmail.com>
2020-01-17 14:37:45 -0500

    Customize Essays admin panels (x3)

--- essays/admin.py
+++ essays/admin.py
@@ -2,7 +2,41 @@ from django.contrib import admin
 from .models import EssayArticle, CuratedSlashdot, CuratedWatchtower, ContentChanges
 # Register your models here.
 
-admin.site.register(EssayArticle)
+
+class EssayArticleAdmin(admin.ModelAdmin):
+    ''' 
+    The guide I followed to implement this classinstance is from 
+    Brad Traversy’s BT Real Estate course on Udemy: Section 6, 
+    Video 39
+    '''
+    list_display = ('title',)
+    list_display_links = ('title',)
+    search_fields = ('content',)
+
+
+class CuratedWatchtowerAdmin(admin.ModelAdmin):
+    ''' 
+    The guide I followed to implement this classinstance is from 
+    Brad Traversy’s BT Real Estate course on Udemy: Section 6, 
+    Video 39
+    '''
+    list_display = ('title',)
+    list_display_links = ('title',)
+    search_fields = ('introduction', 'conclusion',)
+
+
+class CuratedSlashdotAdmin(admin.ModelAdmin):
+    ''' 
+    The guide I followed to implement this classinstance is from 
+    Brad Traversy’s BT Real Estate course on Udemy: Section 6, 
+    Video 39
+    '''
+    list_display = ('title',)
+    list_display_links = ('title',)
+    search_fields = ('introduction', 'conclusion',)
+
+
+admin.site.register(EssayArticle, EssayArticleAdmin)
 admin.site.register(CuratedSlashdot)
 admin.site.register(CuratedWatchtower)
 admin.site.register(ContentChanges)
