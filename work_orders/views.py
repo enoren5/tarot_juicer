75190cad951fb61b2288726daee614437df032ea
Charles Robert Calcifer <coffee.drinker.daniel@gmail.com>
2020-01-23 08:07:02 -0500

    Add 2nd work order+reorganize work order views/urls

--- work_orders/views.py
+++ work_orders/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
 
-def work_orders(request):
-    return render(request, 'work_orders/first.html')
+def first_work_order(request):
+    return render(request, 'work_orders/first_work_order.html')
+
+
+def second_work_order(request):
+    return render(request, 'work_orders/second_work_order.html')
