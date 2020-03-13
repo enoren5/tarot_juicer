2f4a673848c73a04d088760c307cab17dd2f27df
Pete Naveen Pearson <coffee.drinker.daniel@gmail.com>
2020-03-13 08:10:01 -0400

    Re-orient SECRET_KEY based on Elder's Udemy course

--- tarot_juicer/settings.py
+++ tarot_juicer/settings.py
@@ -23,6 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
+# SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
