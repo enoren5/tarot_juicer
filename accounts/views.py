cb088caf498061a1a4fd4cf688720ac08e9182db
Pete Naveen Pearson <coffee.drinker.daniel@gmail.com>
2020-05-01 11:57:53 -0400

    Add 'gateway' ('home') menu option in navbar

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages, auth
 from django.contrib.auth.models import User
-# Create your views here.
 
 
 def register(request):
