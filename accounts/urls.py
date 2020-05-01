c94c18effc5c6abf82b36c38e3a21bd86441ef81
Pete Naveen Pearson <coffee.drinker.daniel@gmail.com>
2020-05-01 10:34:08 -0400

    Swap 'login' and 'portal' for 'gateway' (re-arranged)

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views  # , include
 
 urlpatterns = [
-    # path('', views.index, name='index'),
-    path('login', views.login, name='login'),
+    path('', views.gateway, name='gateway'),
     path('register', views.register, name='register'),
     path('logout', views.logout, name='logout'),
-    path('dashboard', views.dashboard, name='dashboard'),
-    path('gateway', views.gateway, name='gateway'),
+    # path('dashboard', views.dashboard, name='dashboard'),
+    # path('', views.index, name='index'),
+    # path('login', views.login, name='login'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -6,12 +6,9 @@ from django.contrib.auth.models import User
 
 def register(request):
     if request.method == "POST":
-        ''' messages.error(request, 'Testing error')
-        return redirect('register') '''
-
         # Get form values
-        first_name = request.POST['first_name']
-        last_name = request.POST['last_name']
+        # first_name = request.POST['first_name']
+        # last_name = request.POST['last_name']
         username = request.POST['username']
         email = request.POST['email']
         password = request.POST['password']
@@ -33,8 +30,8 @@ def register(request):
                         username=username,
                         password=password,
                         email=email,
-                        first_name=first_name,
-                        last_name=last_name
+                        # first_name=first_name,
+                        # last_name=last_name
                     )
                     # Login after register
                     ''' auth.login(request, user)
@@ -42,10 +39,8 @@ def register(request):
                     return redirect('index')'''
                     user.save()
                     user.success(
-                        request, "You are now registered and can now log in"
-                    )
+                        request, "You are now registered and can now log in")
                     return redirect('login')
-
         else:
             messages.error(request, 'Passwords do not match')
             return redirect('register')
@@ -53,6 +48,7 @@ def register(request):
         return render(request, 'accounts/register.html')
 
 
+'''
 def login(request):
     if request.method == "POST":
         username = request.POST['username']
@@ -67,15 +63,28 @@ def login(request):
             return redirect('login')
     else:
         return render(request, 'accounts/login.html')
+'''
 
 
 def logout(request):
-    return redirect(request, 'index')
+    return redirect(request, 'landings/gateway.html')
 
 
-def dashboard(request):
-    return render(request, 'accounts/dashboard.html')
+'''def dashboard(request):
+    return render(request, 'landings/portal.html')'''
 
 
 def gateway(request):
-    return render(request, 'landings/gateway.html')
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are now logged in!')
+            return redirect('portal')
+        else:
+            messages.error(request, 'Invalid credentials')
+            return render(request, 'landings/gateway.html')
+    else:
+        return render(request, 'landings/gateway.html')
