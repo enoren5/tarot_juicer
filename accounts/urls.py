6fafffb5d7235649afae8e26206fd155476aca9d
= <araariadem0@gmail.com>
2020-05-07 18:49:34 +0100

    add dashboard path

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('register', views.register, name='register'),
     path('logout', views.logout, name='logout'),
-    # path('dashboard', views.dashboard, name='dashboard'),
+    path('dashboard', views.dashboard, name='dashboard'),
     # path('', views.index, name='index'),
     path('login', views.login, name='login'),
 ]
