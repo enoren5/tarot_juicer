e687733de2cc258fc1b7db4dc6ba45323932b6f0
slipsnip <eschmichael@yandex.com>
2020-05-10 23:34:48 -0400

    finish cleanit script

--- scripts/cleanit.sh
+++ scripts/cleanit.sh
@@ -4,10 +4,11 @@
 # why not python? this is faster and more efficient
 # sed 's/\s\{2,\}//g; /^[[:space:]]*$/d' $1 | awk '/models/'
 awk '
-BEGIN { FS = "=" }
+BEGIN { FS = "="
+        OFS = "#"
+}
 {
         if ($1 ~ "models.Model"){
-                print $0;
                 match($0, "[A-Z][a-z]+[[:punct:]]");
                 model = substr($0,RSTART,RLENGTH - 1)
                 if (prev_model != model) {
@@ -16,21 +17,29 @@ BEGIN { FS = "=" }
                 }
         } else if (match($0, "models.[a-zA-Z]+[[:punct:]]") && !begin) {
                 begin = 1
-                field = $1
+                field = substr($1, 0, length($1) - 1)
                 split($2, tmp, ".")
                 type = tmp[2]
                 if (match($0, /models\.[A-Za-z]+\(.*\)/)) {
-                        print field, substr($0, RSTART+7, RLENGTH-2)
+                        typespec = substr($0, RSTART+7, RLENGTH-2)
+                        split(typespec, tmp, /\(/)
+                        spec = substr(tmp[2], 0, length(tmp[2]) -1)
+                        gsub(/,\s/, ",", spec)
+                        if (spec)
+                                print field, tmp[1], spec
+                        else
+                                print field, tmp[1]
                         begin = 0
                 }
         } else if (begin) {
                 type = type $0
                 if ($0 ~ /.*\)$/) {
                         begin = 0
-                        gsub(/\s{2,}/, " ", type)
+                        gsub(/\s{2,}/, "", type)
+                        gsub(/,\s/, ",", type)
                         print field, type
                         type = ""
                 } 
         }
-}' generators/models.py
+}' $1 | tr -d '(' | tr -d ')'
 
