f156844894c1ace25b8ad151f81387af00358fd7
stefanomunarini <ssteee@gmail.com>
2020-05-14 17:59:50 +0100

    Remove registration page from authentication control

--- tarot_juicer/middlewares.py
+++ tarot_juicer/middlewares.py
@@ -5,7 +5,7 @@ from django.urls import reverse
 def authentication_middleware(get_response):
 
     def middleware(request):
-        if not request.user.is_authenticated and request.path != reverse('index'):
+        if not request.user.is_authenticated and request.path not in [reverse('index'), reverse('register')]:
             return HttpResponseRedirect(reverse('index'))
 
         response = get_response(request)
