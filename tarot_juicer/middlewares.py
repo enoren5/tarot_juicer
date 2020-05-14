36470a685e485af38b801203853efe689440ebeb
stefanomunarini <ssteee@gmail.com>
2020-05-14 18:30:01 +0100

    Update middleware to check if Authentication is enabled or disabled by admin

--- tarot_juicer/middlewares.py
+++ tarot_juicer/middlewares.py
@@ -1,12 +1,20 @@
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
+from accounts.models import AuthToggle
+
 
 def authentication_middleware(get_response):
-
     def middleware(request):
-        if not request.user.is_authenticated and request.path not in [reverse('index'), reverse('register')]:
-            return HttpResponseRedirect(reverse('index'))
+        auth_toggle = AuthToggle.objects.first()
+        if not auth_toggle.active:  # authentication NOT required
+            if request.path == reverse('index'):
+                return HttpResponseRedirect(reverse('portal'))
+        else:  # authentication required
+            if not request.user.is_authenticated and \
+                    request.path not in [reverse('index'), reverse('register')] and \
+                     not request.path.startswith(reverse('admin:index')):
+                return HttpResponseRedirect(reverse('index'))
 
         response = get_response(request)
 
