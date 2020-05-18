c825c2cb771032f31a62a0c3ffcec6f81cc312a8
slipsnip <eschmichael@yandex.com>
2020-05-17 23:38:35 -0400

    move models to django and remove heroku from config

--- scripts/injector/config.py
+++ scripts/injector/config.py
@@ -27,26 +27,16 @@ class DjangoConfig():
     project = attrib(default='tarot_juicer')
     app = attrib(default='generators')
     model = attrib(default='Generator')
+    models = attrib(default=attr_factory(dict))
 
     def __str__(self):
         attr_str_(self)
         
-    
-
-@attrs
-class HerokuConfig():
-    project = attrib(default='tarot-juicer-in-production')
-
-    def __str__(self):
-        attr_str_(self)
-    
 
 @attrs
 class Config():
     django = attrib(default=attr_factory(DjangoConfig))
-    heroku = attrib(default=attr_factory(HerokuConfig))
     database = attrib(default=attr_factory(dict))
-    models = attrib(default=attr_factory(dict))
 
     def __str__(self):
         attr_str_(self)
@@ -57,7 +47,6 @@ def load_config_file(file='injector.toml'):
         with Path(CONFIG_FILE).open() as config_toml:
             config = toml.load(config_toml)
             CONFIG.django = DjangoConfig(**config['django'])
-            CONFIG.heroku = HerokuConfig(**config['heroku'])
     except FileNotFoundError:
         # accept defaults
         pass
@@ -76,7 +65,6 @@ def config():
         # ask the questions
         django_projects = util.django.get_projects()
         django_apps = util.django.get_apps()
-        django_models = util.django.get_models().keys()
         CONFIG.django.project = option_prompt(django_projects, 
                 'Which Django project?',
                 show_menu=True,
@@ -85,6 +73,7 @@ def config():
                 'Which Django app?',
                 show_menu=True,
                 title='[DJANGO APPS]')
+        django_models = util.django.get_models(CONFIG.django.app).keys()
         CONFIG.django.model = option_prompt(django_models,
                 'Which Model are we targeting?',
                 show_menu=True,
@@ -92,33 +81,7 @@ def config():
 
 
     # get database into config
-        
     databases = util.django.get_databases(CONFIG.django.project)
     database_id = option_prompt(databases.keys(), 'Which database are we working with?', show_menu=True, title='[DATABASES]')
     CONFIG.database = databases[database_id]
-    CONFIG.models = util.django.get_models(CONFIG.django.app)
-    
-
-
-    # not using defaults, ask the user questions
-    # if not defaults:
-    #     confirm_project = 'no'
-    #     while confirm_project == 'no':
-    #         confirm_project = get_option(['yes', 'no'], f'is {config.heroku_project} the heroku project name?')
-    #         if confirm_project != 'yes':
-    #             config.heroku_project = input('What is the name of the heroku project? ')
-    #     config.database = get_database_config(heroku_project, default=False)
-    #     django_apps = get_apps()
-    #     config.django_app = get_option(django_apps, f'What is our django app? ', title='[APPS]')
-    #     config.models = get_models(config.django_app)
-    #     config.model = get_option(list(config.models.keys()), 'Which model do you want? ', title='[MODELS]')
-    # else:
-    #     config.models = get_models(config.django_app)
-    #     config.database = get_database_config(config.heroku_project, default=True)
-
-    # if verbose:
-    #     print('')
-    #     menu('[CONFIGURATION]')
-    #     print(config)
-
-    return config
+    CONFIG.django.models = util.django.get_models(CONFIG.django.app)
