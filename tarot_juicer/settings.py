a316681220387ebc9b697a8b490275146f0f0b20
Pete Naveen Pearson <coffee.drinker.daniel@gmail.com>
2020-11-19 06:01:44 -0500

    Correct statis files reference

--- tarot_juicer/settings.py
+++ tarot_juicer/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.static',
 ]
 
 MIDDLEWARE = [
