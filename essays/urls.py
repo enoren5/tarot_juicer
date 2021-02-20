5e0c5b828e91db9e4c2a42f090497a572cf46b56
UmarGit <umarsheikh303@gmail.com>
2021-02-20 21:24:37 +0500

    Updated 404s
    Updated 404s

--- essays/urls.py
+++ essays/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path('article/objections/', views.objections, name='objections'),
     path('article/content_changelog/',
          views.content_changelog, name='content_changelog'),
-    path('article/watchtower/', views.watchtower, name='watchtower'),
-    path('article/slashdot/', views.slashdot, name='slashdot'),
+    path('article/watchtower', views.watchtower, name='watchtower'),
+    path('article/slashdot', views.slashdot, name='slashdot'),
     path('article/bibliography/', views.bibliography, name='bibliography'),
     path('article/all_content_dump/',
          views.all_content_dump, name='all_content_dump')
