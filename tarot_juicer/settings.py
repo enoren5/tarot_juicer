39fee5baa365c8255981fd637ead870579352e8c
Pete Naveen Pearson <coffee.drinker.daniel@gmail.com>
2021-02-26 10:03:10 -0500

    Add dynamic DEBUG env variable

--- tarot_juicer/settings.py
+++ tarot_juicer/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
