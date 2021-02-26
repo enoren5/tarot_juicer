83b409f81110957163f2f935ae23f3b406af8abd
Pete Naveen Pearson <coffee.drinker.daniel@gmail.com>
2021-02-26 12:58:09 -0500

    Experiment with DATABASE_URL

--- tarot_juicer/settings.py
+++ tarot_juicer/settings.py
@@ -89,6 +89,7 @@ WSGI_APPLICATION = 'tarot_juicer.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+'''
 SELECTED_DB = ""
 
 VALUE = os.getenv('SELECT_DB')
@@ -97,10 +98,13 @@ if VALUE == "0":
     SELECTED_DB = "HEROKU_POSTGRESQL_SILVER_URL"
 elif VALUE == "1":
     SELECTED_DB = "HEROKU_POSTGRESQL_NAVY_URL"
+'''
+
+DATABASE_URL = ""
 
 DATABASES = {
     'default': dj_database_url.config(
-        env=SELECTED_DB,
+        env=DATABASE_URL,
         default='sqlite:///'+os.path.join(BASE_DIR, 'db.sqlite3'), 
         conn_max_age=600)
     }
