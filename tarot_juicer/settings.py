d0e8220572becb8ed7136a1cf19d79c4b9cbcb57
Pete Naveen Pearson <coffee.drinker.daniel@gmail.com>
2021-03-14 22:34:43 -0400

    Revert double negation of DJANGO_DEBUG

--- tarot_juicer/settings.py
+++ tarot_juicer/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-if os.environ.get('DJANGO_DEBUG', '') == 'True':
+if os.environ.get('DJANGO_DEBUG', '') != 'False':
     # These are testing settings:
     DEBUG = True 
     SECURE_HSTS_SECONDS = 10
