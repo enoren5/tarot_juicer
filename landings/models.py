b9ed312e5892e13a5fb91ecb06e4c812e71635f6
Pete Naveen Pearson <coffee.drinker.daniel@gmail.com>
2021-08-06 08:44:37 -0400

    Wire up `title` data to about and essay list templates

--- landings/models.py
+++ landings/models.py
@@ -4,14 +4,16 @@ from django.db import models
 
 
 class EssayList(models.Model):
-    text_content = models.TextField(blank=True)
-
-    '''def __str__(self):
-        return self.title'''
+    title = models.CharField(max_length=256, default='Essay list and how to use this website')
+    text_content = models.TextField(blank=True)    
+    
+    def __str__(self):
+        return self.title
 
 
 class AboutContent(models.Model):
-    text_content = models.TextField(blank=True)
-
-    '''def __str__(self):
-        return self.title'''
+    title = models.CharField(max_length=256, default='What is going on here?')
+    text_content = models.TextField(blank=True)    
+    
+    def __str__(self):
+        return self.title
