0ab96ead9cb5e6c6ec8c0d66b07976915cb0e35e
Pete Naveen Pearson <warden496@protonmail.com>
2021-12-16 19:11:26 -0500

    Enable Email app in Admin Dashboard

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from accounts.models import AuthToggle, PassPhrase
+from accounts.models import AuthToggle, PassPhrase, Email
 
 admin.site.register(AuthToggle)
 admin.site.register(PassPhrase)
+admin.site.register(Email)
