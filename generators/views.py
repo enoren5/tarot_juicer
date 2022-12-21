7c207e02947da073f2bbe3906faac012d1a9e174
AbubakarA-Dot <abubakarbryar@gmail.com>
2022-12-21 09:49:27 +0500

    Removed is_published from generators app

--- generators/views.py
+++ generators/views.py
@@ -37,13 +37,14 @@ def getPrevNext(array, element):
 
 
 def tarot_key(request, generator_number):
-    try:
-        generator_obj = Generator.objects.get(is_published=True)
-    except Generator.DoesNotExist:
-        raise Http404("Generator Doesn't exists!")
+    # try:
+    #     generator_obj = Generator.objects.get(is_published=True)
+    # except Generator.DoesNotExist:
+    #     raise Http404("Generator Doesn't exists!")
     try:
         generator = Generator.objects.filter(
             number=generator_number).first()
+        print("\n Generator :", generator)
         next_card_number = Generator.objects.order_by('?').first().number
 
         generator_array = []
@@ -55,7 +56,7 @@ def tarot_key(request, generator_number):
         
         cards = Generator.objects.order_by('number')
         context = {
-            'generator_obj': generator_obj,
+            # 'generator_obj': generator_obj,
             'generator': generator,
             'cards': cards,
             'next_card_number': next_card_number,
